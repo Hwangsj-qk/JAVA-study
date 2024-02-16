8b3f55410aac741dd1e333be33f7f811db09f62e
Hwangsj-qk <sujeong7191@naver.com>
2024-02-17 00:40:58 +0900

    변수 선언

---
+++ src/JAVA_240207/variable/Ex1.java
@@ -0,0 +1,15 @@
+package JAVA_240207.variable;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        // 연습문제 1: 5와 3을 다른 숫자로 한 번에 변경할 수 있게 변수로 선언해주세요.
+        int x = 5;
+        int y = 3;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x / y);
+        System.out.println(x * y);
+
+    }
+}
