edb99d9d76e106816d222d61cc823e467f615d32
Hwangsj-qk <sujeong7191@naver.com>
2024-02-22 12:30:52 +0900

    상속(inheritance) 공부

---
+++ src/java_240222/inheritance/CellPhone.java
@@ -0,0 +1,12 @@
+package java_240222.inheritance;
+
+public class CellPhone {
+    // 필드
+    String model;
+    String color;
+
+    // 메서드
+    void bell() {
+        System.out.println("벨이 울립니다.");
+    }
+}
---
+++ src/java_240222/inheritance/DmbCellPhone.java
@@ -0,0 +1,11 @@
+package java_240222.inheritance;
+
+public class DmbCellPhone extends CellPhone {
+    // 필드(super 클래스의 필드는 모두 가지고)
+    int channel;    // 새로운 필드 추가
+
+    // 메서드 super class의 필드를 가지고
+    void turnOnDmb() {  // 새로운 기능 추가
+        System.out.println(channel + "번 DMB방송을 수신합니다. ");
+    }
+}
