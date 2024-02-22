0adc691f1fdac135105b30bff3dec8fa07296df1
Hwangsj-qk <sujeong7191@naver.com>
2024-02-22 16:32:16 +0900

    test1 연습

---
+++ src/test1_practice/Multiply.java
@@ -0,0 +1,14 @@
+package test1_practice;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int multiply = 1;
+        for (int i = 1; i <= 9; i++) {
+            System.out.println();
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i +  " * " +  j +  " = " + i*j);
+            }
+
+        }
+    }
+}
---
+++ src/test1_practice/Switch.java
@@ -0,0 +1,21 @@
+package test1_practice;
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        // condition - ex4
+
+        Scanner scanner = new Scanner(System. in);
+        System.out.print("첫번째 숫자를 입력하세요: ");
+        int num1 = scanner.nextInt();
+        System.out.print("두번재 숫자를 입력하세요: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("연산자를 입력하세요: ");
+        String operator = scanner.nextLine();
+
+
+
+    }
+}
