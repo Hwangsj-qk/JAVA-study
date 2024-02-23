ade70a7e7fe4c43d24dab3846e4862766be77f5e
Hwangsj-qk <sujeong7191@naver.com>
2024-02-23 13:54:03 +0900

    test1

--- src/java_Test/Test2.java
+++ src/java_Test/Test2.java
@@ -29,10 +29,10 @@ public class Test2 {
                 break;
                 
             case "/" :
-                result = num1 / num2;
+                result =(double) num1 / num2;
                 if(num2 == 0) {
                     System.out.println("0으로 나눌 수 없습니다. ");
-                    return;
+                    break;
                 }
         }
         System.out.println("결과: " + result);
--- src/java_Test/Test3.java
+++ src/java_Test/Test3.java
@@ -8,25 +8,32 @@ import java.util.Scanner;
 public class Test3 {
     public static void main(String[] args) {
         Random random = new Random();
-        int answer = (int)(Math.random() * 100 ) + 1;
+        int answer = (int) (Math.random() * 100) + 1;
+
+        System.out.println(answer);
 
         Scanner scanner = new Scanner(System.in);
 
         int tryCount = 0;
 
         System.out.println("1~ 100까지 숫자 중 맞춰보세요");
-        System.out.print("숫자를 입력하세요: ");
-        int tryNumber = scanner.nextInt();
-        tryCount++;
+        int tryNumber;
+
         do {
-           if(answer < tryNumber) {
-               System.out.println("오답! down 하세요.");
-           } else {
-               System.out.println("오답! up 하세요. ");
-           }
 
+            System.out.println("숫자를 입력하세요");
+            tryNumber = scanner.nextInt();
+            tryCount++;
+
+            if (answer < tryNumber) {
+                System.out.println("오답! down 하세요.");
+
+            } else if (answer > tryNumber) {
+                System.out.println("오답! up 하세요. ");
+
+            }
+            break;
         } while (answer != tryNumber);
-
-        System.out.println("정답!" + tryCount + "번에 맞추셨네요!");
-    }
-}
+                System.out.println("정답!" + tryCount + "번에 맞추셨네요!");
+            }
+        }
