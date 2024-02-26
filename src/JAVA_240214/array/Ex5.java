101b20089b81c0f557c190d568783c3dcffb16b3
Hwangsj-qk <sujeong7191@naver.com>
2024-02-26 11:50:00 +0900

    조건문 예제

--- src/JAVA_240214/array/Ex5.java
+++ src/JAVA_240214/array/Ex5.java
@@ -16,9 +16,11 @@ public class Ex5 {
         // 점수 입력
         for (int[] students : scores) {
             System.out.println("학생의 성적을 입력하세요");
-            for (int i : students) {
+            for (int i = 0; i < students.length; i++) {
                 System.out.println("점수: ");
                 students[i] = scanner.nextInt();
+            } {
+
             }
         }
         // 총점과 평균 구하기
