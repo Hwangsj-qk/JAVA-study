f43f2b41463bcbde7f9b985818d40d0eb0c607f8
Hwangsj-qk <sujeong7191@naver.com>
2024-03-04 16:29:57 +0900

    예외 처리 연습문제

---
+++ src/java_240304/exception/ex/InvaildEmailException.java
@@ -0,0 +1,7 @@
+package java_240304.exception.ex;
+
+public class InvaildEmailException extends Exception{
+    public InvaildEmailException(String message) {
+        super(message);
+    }
+}
---
+++ src/java_240304/exception/ex/InvalidAgeException.java
@@ -0,0 +1,8 @@
+package java_240304.exception.ex;
+
+public class InvalidAgeException extends Exception{
+
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
