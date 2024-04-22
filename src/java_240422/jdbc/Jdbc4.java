f21eb01c001b3038c86046ff88f9b3c188c392a6
Hwangsj-qk <sujeong7191@naver.com>
2024-04-22 13:36:34 +0900

    JDBC

---
+++ src/java_240422/jdbc/Jdbc4.java
@@ -0,0 +1,29 @@
+package java_240422.jdbc;
+
+import java.sql.*;
+
+public class Jdbc4 {
+    // try-with-resources 구문으로 자원 정리하ㅣㄱ
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/scott";
+        String user = "root";
+        String password = "1234";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM emp ")) {
+
+            while (rs.next()) {
+                int eno = rs.getInt(1);// 첫번재 컬럼을 정수타입으로 반환
+                String ename = rs.getString(2);     // 두번째 컬럼을 문자열 타입으로
+                Date hiredate = rs.getDate("hireDate");
+                System.out.println("사원번호: " + eno + ", 이름: " + ename + ", 입사일: " + hiredate);
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        // 5. 자원 종료
+        // close 메서드 사용하지 않아도, try 구문이 끝나면 안전하게 종료함
+    }
+}
---
+++ src/java_240422/jdbc/ex/Ex1.java
@@ -0,0 +1,29 @@
+package java_240422.jdbc.ex;
+
+import java.sql.*;
+
+public class Ex1 {
+    // 연습문제.
+    // scott DB에서 10번 부서에 일하는 직원의 이름과 직무, 연봉을 조회하여 출력하세요.
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/scott";
+        String user = "root";
+        String password = "1234";
+
+        String query = "SELECT ename, job, sal FROM emp WHERE deptno = 10";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            ) {
+            while (rs.next()) {
+                System.out.println("이름: " + rs.getString("ename")
+                        + " 직무: " + rs.getString("job")
+                        + " 연봉: " +rs.getInt("sal"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
