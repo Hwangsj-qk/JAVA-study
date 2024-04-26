c817308c7eddc3a1c075533a6223d127f77d4d12
Hwangsj-qk <sujeong7191@naver.com>
2024-04-26 10:04:25 +0900

    Jdbc(동적 INSERT, 안전 UPDATE) 연습문제

--- src/java_240422/jdbc/ex/Ex3.java
+++ src/java_240422/jdbc/ex/Ex3.java
@@ -36,50 +36,67 @@ public class Ex3 {
             String password = "1234";
             Scanner scanner = new Scanner(System.in);
 
-            // 사용자 정보 입력받기(엔터를 받을 경우 패스_
+            // 사용자 정보 입력받기(엔터를 받을 경우 패스)
             System.out.print("업데이터할 userId를 입력하세요: ");
-            String userId = scanner.next();
+            String userId = scanner.nextLine();
             System.out.print("새로운 사용자 이름을 입력하세요: ");
-            String userName = scanner.next();
+            String userName = scanner.nextLine();
             System.out.print("새로운 비밀번호를 입력하세요: ");
-            String userPassword = scanner.next();
+            String userPassword = scanner.nextLine();
             System.out.print("새로운 나이를 입력하세요: ");
-            String userAge= scanner.next();
+            String userAge= scanner.nextLine();
             System.out.print("새로운 이메일 주소를 입력하세요: ");
-            String userEmail = scanner.next();
+            String userEmail = scanner.nextLine();
 
-            try(Connection conn = DriverManager.getConnection(url,user,password)) {
+            /* next()와 nextLine()의 차이점
+                - next()는 개행문자(ex. 줄바꿈)를 무시하고 입력을 받음
+                - nextLine()은 한줄 단위로 입력받음
+                    -> 현재 이 문제에서는 줄바꿈을 선택할 경우 넘어가야하므로 nextline()을 사용하는 것이 맞다.
+             */
+
+            try(Connection conn = DriverManager.getConnection(url, user, password)) {
                 // 쿼리문 Update 고정적인 부분
                 StringBuilder query = new StringBuilder("UPDATE users SET ");
                 // 콤마 SQL문법을 위한 플래그
                 boolean isFirst = true;
 
                 // 동적으로 쿼리문 변경
-                // "컬럼명1 =값1, 컬럼명2= 값2,..."
+                // "컬럼명1=값1, 컬럼명2=값2,..."
                 // "WHERE userId = ?";
-                // 값을 입력받지 않았을 경우
+                // 값을 입력받지 않았을 경우 ""
 
-
-                if(!userPassword.isEmpty()) {
-                    // 값이 입력되었을 경우
-                    if(! isFirst) query.append(", ");        // 첫번째 컬럼이 아닌 경우만 콤마 추가
-                    query.append("password = ?");
-                    isFirst = false;        // 첫번째 컬럼 사용됨
+                // 값이 입력되었을 경우
+                if(!userName.isEmpty()) {
+                    query.append("userName = ?");
+                    isFirst = false;
                 }
 
-                if (!userAge.isEmpty()) {
-                    if(!isFirst) query.append(", ");
-                    query.append("age = ?");
-                    isFirst = false;
+                // 값이 입력되었을 경우
+                if(!userPassword.isEmpty()) {
+                    if(!isFirst) {
+                        query.append(", ");             // 첫번째 컬럼이 아닌 경우만 콤마 추가
+                    }
+                    query.append("password = ? ");
+                    isFirst = false;                    // 첫번째 컬럼 사용됨
                 }
 
                 if(!userAge.isEmpty()) {
-                    if(!isFirst) query.append(", ");
-                    query.append("email = ?");
-                    isFirst = false;
+                    if(!isFirst) {
+                        query.append(", ");             // 첫번째 컬럼이 아닌 경우만 콤마 추가
+                    }
+                    query.append("age = ? ");
+                    isFirst = false;                    // 첫번째 컬럼 사용됨
                 }
 
-                // 최종 조건 쿼리문(고정)
+                if(!userEmail.isEmpty()) {
+                    if(!isFirst) {
+                        query.append(", ");             // 첫번째 컬럼이 아닌 경우만 콤마 추가
+                    }
+                    query.append("email = ? ");
+                    isFirst = false;                    // 첫번째 컬럼 사용됨
+                }
+
+                // 최종 조건 쿼리문 (고정)
                 query.append("WHERE userId = ?");
 
                 // 쿼리문 확인
@@ -89,31 +106,27 @@ public class Ex3 {
                 String sql = query.toString();
                 PreparedStatement pstmt = conn.prepareStatement(sql);
 
+                // 동적인 값(?)을 부여
                 int index = 1;
-                // 입력받은 값이 있을 경우만 인덱스를 사용하고 인덱스 번호를 증가
-                if (!userName.isEmpty()) pstmt.setString(index++, userName);
-                if (!userPassword.isEmpty()) pstmt.setString(index++, userPassword);
-                if (!userAge.isEmpty()) pstmt.setInt(index++, Integer.parseInt(userAge));
-                if (!userEmail.isEmpty()) pstmt.setString(index++, userEmail);
-                pstmt.setString(index++, userId);       // userId는 무조건 존재
+                // 입력받은 값이 있을 경우만 인덱스를 사용하고 인덱스 번호 증가 (후위 연산자 사용)
+                if(!userName.isEmpty()) pstmt.setString(index++, userName);
+                if(!userPassword.isEmpty()) pstmt.setString(index++, userPassword);
+                if(!userAge.isEmpty()) pstmt.setInt(index++, Integer.parseInt(userAge));
+                if(!userEmail.isEmpty()) pstmt.setString(index++, userEmail);
+                pstmt.setString(index++, userId);       // userId는 반드시 존재
 
-                // SQL 문 실행
+                // SQL문 실행
                 int rows = pstmt.executeUpdate();
 
-                // 결과 로그
+                // 결과 로그 출력
                 if(rows == 1) {
                     System.out.println(userId + "의 정보가 업데이트 되었습니다. ");
                 } else {
-                    System.out.println("업데이트 되지 않았습니다. ");
+                    System.out.println("오류 발생 : " + userId + "의 정보가 업데이트에 실패하였습니다. ");
                 }
-
                 pstmt.close();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
-
-
-
-
         }
 }
--- src/java_240422/jdbc/ex/Ex4.java
+++ src/java_240422/jdbc/ex/Ex4.java
@@ -1,42 +1,83 @@
 package java_240422.jdbc.ex;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Ex4 {
-     /*
+  /*
     연습문제 2: 사용자 삭제
     요구사항: 사용자로부터 userId를 입력받아 해당 userId를 가진 사용자의 데이터를 데이터베이스에서 삭제하세요.
 
     - 사용자로부터 userId를 입력받습니다.
     - 해당 userId를 가진 사용자의 데이터를 users 테이블에서 삭제합니다.
-      */
+     */
+
+    /* 삭제 출력예시
+    삭제할 사용자 ID를 입력하세요: abcdef
+    해당 사용자 ID를 가진 사용자가 없습니다.
+
+    삭제할 사용자 ID를 입력하세요: spring
+    발견된 사용자 정보:
+    ID: spring
+    이름: 봄봄
+    이메일: spring@abc.com
+    나이: 25
+    이 사용자를 정말 삭제하시겠습니까? (y/n): n
+
+    삭제할 사용자 ID를 입력하세요: spring
+    발견된 사용자 정보:
+    ID: spring
+    이름: 봄봄
+    이메일: spring@abc.com
+    나이: 25
+    이 사용자를 정말 삭제하시겠습니까? (y/n): y
+    사용자 ID spring의 정보가 성공적으로 삭제되었습니다.
+    */
+
      public static void main(String[] args) {
          String url = "jdbc:mysql://localhost:3306/jdbc";
          String user = "root";
          String password = "1234";
-
-         String sql = "DELETE FROM users WHERE userId = ? ";
-
          Scanner scanner = new Scanner(System.in);
 
+         // 사용자 정보 입력받기 (엔터를 받을 경우 패스)
          System.out.print("삭제할 userId를 입력하세요: ");
-         String userId = scanner.next();
+         String userId = scanner.nextLine();
 
+         // DB 연결
          try (Connection conn = DriverManager.getConnection(url, user, password);
-              PreparedStatement pstmt = conn.prepareStatement(sql);
-         ){
+              PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM users WHERE userId = ?");
+              PreparedStatement pstmtDelete = conn.prepareStatement("DELETE FROM users WHERE userId = ?");
+         ) {
+
+             // SELECT 문으로 존재여부 확인
              pstmt.setString(1, userId);
+             ResultSet rs = pstmt.executeQuery();
 
-             int rows = pstmt.executeUpdate();
+             if (rs.next()) {
+                 System.out.println("ID : " + rs.getString("userId"));
+                 System.out.println("이름 : " + rs.getString("username"));
+                 System.out.println("이메일 : " + rs.getString("email"));
+                 System.out.println("나이 : " + rs.getInt("age"));
 
-             if (rows == 1) {
-                 System.out.println("사용자 ID " + userId + "의 정보가 성공적으로 삭제되었습니다. " );
+                 // 삭제 여부 확인하기
+                 System.out.println("정말로 삭제하시겠습니까? (y/n)");
+                 String response = scanner.nextLine();
+
+                 if (response.equals("y")) {
+                     pstmtDelete.setString(1, userId);      // 동적 쿼리 완성
+                     int rows = pstmtDelete.executeUpdate();
+
+                     if (rows == 1) {
+                         System.out.println("성공적으로 삭제되었습니다. ");
+                     } else {
+                         System.out.println("오류 발생 : 삭제에 실패하였습니다. ");
+                     }
+                 } else {
+                     System.out.println("삭제가 취소되었습니다. ");
+                 }
              } else {
-                 System.out.println("해당 사용자 ID를 가진 사용자가 없습니다. ");
+                 System.out.println("해당 사용자 ID가 없습니다. ");
              }
 
          } catch (SQLException e) {
